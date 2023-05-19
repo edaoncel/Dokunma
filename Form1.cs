a89f8097d56cbf843dfc7ecb5ca39b4c3136b660
eda oncel <edaoncel@email.com>
2023-05-19 14:29:24 +0300

    Dokunma Projesi

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dokunmaaa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button4_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button5_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kazandınız");
+        }
+
+        private void button6_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("hile yaptınız");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
